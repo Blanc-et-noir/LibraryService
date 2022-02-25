46dd31c177e7d2d45197c74b021e377ecbf7105e
Blanc-et-noir <dign9060@gmail.com>
2022-02-25 20:57:41 +0900

    주석 추가

--- src/main/java/com/spring/LibraryService/controller/BoardController.java
+++ src/main/java/com/spring/LibraryService/controller/BoardController.java
@@ -23,12 +23,18 @@ import com.spring.LibraryService.encrypt.SHA;
 import com.spring.LibraryService.service.BoardService;
 import com.spring.LibraryService.vo.CustomerVO;
 
+//게시판과 관련된 요청을 처리할 컨트롤러
 @Controller("board")
 public class BoardController {
 	@Autowired
 	private BoardService boardService;
+	//자유, 정보, 문의게시판들의 베이스 디렉토리
 	private static String REPOSITORY_PATH = "D:\\LibraryService\\board\\";
 	
+	
+	//============================================================================================
+	//클라이언트가 요청한 게시판 종류에 따라 서로 다른 파라미터를 mav 객체에 할당함. 
+	//============================================================================================
 	@RequestMapping(value="/board/boardForm.do")
 	public ModelAndView boardForm(HttpServletRequest request, HttpServletResponse response) {
 		String BOARD = request.getParameter("BOARD");
@@ -45,10 +51,15 @@ public class BoardController {
 			mav.addObject("BOARD", "qna_board");
 			return mav;
 		}else {
+			//게시판의 종류가 잘못되었을때, 메인화면으로 이동
 			return new ModelAndView("redirect:/customer/mainForm.do");
 		}
 	}
 	
+	
+	//============================================================================================
+	//게시글 작성 화면요청을 처리하는 메소드, 게시판 종류가 잘못되어있으면 메인화면으로 이동함.
+	//============================================================================================
 	@RequestMapping(value="/board/addArticleForm.do")
 	public ModelAndView LOGONMAV_addArticleForm(HttpServletRequest request, HttpServletResponse response) {
 		String BOARD = request.getParameter("BOARD");
@@ -71,6 +82,10 @@ public class BoardController {
 	}
 	
 	
+	
+	//============================================================================================
+	//게시글 작성 요청을 처리하는 메소드
+	//============================================================================================
 	@RequestMapping(value="/board/addArticle.do")
 	@ResponseBody
 	public HashMap LOGONMAP_addArticle(MultipartHttpServletRequest request, HttpServletResponse response){
@@ -101,9 +116,16 @@ public class BoardController {
 				String fileName = iterator.next();
 				MultipartFile mFile = request.getFile(fileName);
 				String originalFileName = mFile.getOriginalFilename();
+				
+				//무작위 SALT값과 SHA512 알고리즘을 이용해 무작위 임시 이름을 얻음
+				//서로 위치는 달라도, 이름은 같은 이미지파일이 있을수 있기 때문에 이름 중복으로 인한 기본키 제약 위반을 회피함
 				String tempFileName = SHA.DSHA512(SHA.getSalt(), SHA.getSalt()).substring(0, 32);
+				
+				//파일의 확장자를 얻고, 새로 발급받은 임시 파일이름에 확장자를 덧붙이고, 아래의 경로에 이미지를 저장함
 				String fileExtension = mFile.getOriginalFilename().substring(mFile.getOriginalFilename().lastIndexOf("."));
 				File dest = new File(REPOSITORY_PATH+"\\"+BOARD+"\\"+ARTICLE_ID+"\\"+tempFileName+fileExtension);
+				
+				//만약 첨부된 파일이 존재하면, 해당 파일에 대한 정보를 DB에 추가하기위해 map 객체에 저장함.
 				HashMap info = new HashMap();
 				info.put("tempFileName",tempFileName+fileExtension);
 				info.put("originalFileName",originalFileName);
@@ -112,6 +134,7 @@ public class BoardController {
 				mFile.transferTo(dest);
 			}
 			
+			//첨부한 파일이 있을 경우에는, 게시글의 첨부파일들의 정보를 DB에 삽입함.
 			if(fileNames.size()>0) {
 				HashMap param = new HashMap();
 				param.put("ARTICLE_ID", ARTICLE_ID);
@@ -127,6 +150,13 @@ public class BoardController {
 			return result;
 		}
 	}
+	
+	
+	
+	
+	//============================================================================================
+	//게시글을 수정하는 메소드, 자신의 게시글이어야만 수정이 가능하다.
+	//============================================================================================
 	@RequestMapping(value="/board/modifyArticle.do")
 	@ResponseBody
 	public HashMap LOGONMAP_modifyArticle(MultipartHttpServletRequest request, HttpServletResponse response) {
@@ -146,7 +176,6 @@ public class BoardController {
 			map.put("ARTICLE_TITLE", request.getParameter("ARTICLE_TITLE"));
 			map.put("ARTICLE_CONTENT", request.getParameter("ARTICLE_CONTENT"));
 			map.put("BOARD", BOARD);
-			map.put("ARTICLE_ID", ARTICLE_ID);
 			map.put("BOARD_FILE", BOARD+"_file");
 			return boardService.modifyArticle(request, map);
 		}catch(Exception e) {
@@ -156,6 +185,14 @@ public class BoardController {
 			return result;
 		}		
 	}
+	
+	
+	
+	
+	//============================================================================================
+	//게시글 목록을 얻는 메소드, 비동기식으로 응답하므로 화면 새로고침이 발생하지 않음. 편의성을 보장하기 위함.
+	//게시글을 날짜순서로 내림차순 정렬했을때 (SECTION-1)*100+(PAGE-1)*10+1 번째 게시글부터 10개의 게시글을 얻음.
+	//============================================================================================
 	@RequestMapping(value="/board/listArticles.do")
 	@ResponseBody
 	public HashMap listArticles(HttpServletRequest request, HttpServletResponse response) throws Exception{
@@ -170,14 +207,19 @@ public class BoardController {
 		map.put("SECTION", request.getParameter("SECTION"));
 		map.put("PAGE", request.getParameter("PAGE"));
 		map.put("BOARD", BOARD);
+		
+		//FLAG는 사용자 ID, 게시글 제목, 게시글 내용중 어떤 것을 기준으로 게시글을 검색할 것인지를 나타냄.
 		map.put("FLAG", request.getParameter("FLAG"));
 		map.put("SEARCH", request.getParameter("SEARCH"));
 		return boardService.listArticles(map);
-
 	}
 
+	
+	
 
-
+	//============================================================================================
+	//특정 게시글을 읽고자 할 때 응답하는 메소드
+	//============================================================================================
 	@RequestMapping(value="/board/viewArticle.do")
 	public ModelAndView viewArticle(HttpServletRequest request, HttpServletResponse response){
 		try {
@@ -185,16 +227,26 @@ public class BoardController {
 			int ARTICLE_ID = Integer.parseInt(request.getParameter("ARTICLE_ID"));
 			if(BOARD==null||(!BOARD.equals("free_board")&&!BOARD.equals("qna_board")&&!BOARD.equals("info_board"))) {
 				return new ModelAndView("redirect:/customer/mainForm.do");
-			}			
+			}
 			HashMap map = new HashMap();
 			map.put("BOARD", BOARD);
 			map.put("ARTICLE_ID", ARTICLE_ID);
+			
+			//게시글 읽기에 성공하면 viewArticle 뷰를 리턴하며, JSP의 JSTL 기능을 이용하여
+			//동적으로 게시글 제목과 내용, 첨부된 이미지들에 대한 처리를 진행한다.
 			return boardService.viewArticle(map);
 		}catch(Exception e) {
 			return new ModelAndView("redirect:/customer/mainForm.do");
 		}
 	}
 	
+	
+	
+	//============================================================================================
+	//게시글을 삭제할때 요청하는 메소드, 해당 게시글이 자신의 게시글이어야만 삭제가 가능하다.
+	//1차적으로 JSTL을 이용하여 삭제버튼을 보여줄지 말지 결정하며, 2차적으로 컨트롤러에서 로그인 여부를 AOP를 이용하여 걸러내고
+	//3차적으로 DELETE를 수행할 때 WHERE에 CUSTOMER_ID와 같아야 삭제가 되도록 조건을 추가했다.
+	//============================================================================================
 	@RequestMapping(value="/board/deleteArticle.do")
 	public ModelAndView LOGONMAV_deleteArticle(HttpServletRequest request, HttpServletResponse response){
 		try {
@@ -202,10 +254,12 @@ public class BoardController {
 			int ARTICLE_ID = Integer.parseInt(request.getParameter("ARTICLE_ID"));
 			if(BOARD==null||(!BOARD.equals("free_board")&&!BOARD.equals("qna_board")&&!BOARD.equals("info_board"))) {
 				return new ModelAndView("redirect:/customer/mainForm.do");
-			}			
+			}
 			HashMap map = new HashMap();
 			map.put("BOARD", BOARD);
 			map.put("ARTICLE_ID", ARTICLE_ID);
+			
+			//게시글 삭제에 성공하면 게시글 목록으로 이동한다.
 			return boardService.deleteArticle(map);
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -213,6 +267,12 @@ public class BoardController {
 		}
 	}
 	
+	
+	
+	
+	//============================================================================================
+	//해당 게시글에 첨부된 이미지들을 서버에서 다운로드 요청을 할 때 응답하는 메소드.
+	//============================================================================================
 	@RequestMapping(value="/board/download.do")
 	public void downloadFile(HttpServletRequest request, HttpServletResponse response){
 		try {
@@ -222,6 +282,8 @@ public class BoardController {
 			if(BOARD==null||(!BOARD.equals("free_board")&&!BOARD.equals("qna_board")&&!BOARD.equals("info_board"))||FILE_TEMP_NAME==null) {
 				return;
 			}
+			
+			//해당 게시글의 이미지파일이 실제로 존재해야만 다운로드가 성공적으로 진행됨.
 			File file = new File(REPOSITORY_PATH+"\\"+BOARD+"\\"+ARTICLE_ID+"\\"+FILE_TEMP_NAME);
 			if(file.exists()) {				
 				response.setHeader("Cache-Control", "no-cache");
--- src/main/java/com/spring/LibraryService/service/BoardService.java
+++ src/main/java/com/spring/LibraryService/service/BoardService.java
@@ -26,8 +26,17 @@ public class BoardService {
 	private BoardDAO boardDAO;
 	private static String REPOSITORY_PATH = "D:\\LibraryService\\board";
 	
+	
+	
+	//============================================================================================
+	//게시글 작성을 수행하는 서비스 메소드
+	//============================================================================================
 	public HashMap addArticle(HashMap map) throws Exception{
+		//게시글 종류에 따라 새 게시글을 위한 새로운 게시글 ID를 얻음.
+		//오라클DBMS 11XE에서는 기본 시퀀스문법만 지원하여 아래와 같이 처리함.
 		map.put("ARTICLE_ID",boardDAO.getNewArticleID(map));
+		
+		//게시글 작성에 성공하면 성공했음을 응답, 실패시에 예외를 발생시킴.
 		boardDAO.addArticle(map);
 		HashMap result = new HashMap();
 		
@@ -37,21 +46,44 @@ public class BoardService {
 		return result;
 	}
 	
+	
+	
+	
 	public void addFiles(HashMap map) throws Exception{
 		boardDAO.addFiles(map);
 	}
 	
+	
+	
+	
+	//============================================================================================
+	//게시글 수정을 수행하는 메소드. 자신의 게시글만 수정할 수 있다.
+	//============================================================================================
 	public HashMap modifyArticle(MultipartHttpServletRequest request, HashMap map) throws Exception{
 		HashMap result = new HashMap();
 		String BOARD = (String)map.get("BOARD");
 		int ARTICLE_ID = (Integer)map.get("ARTICLE_ID");
+		
+		//먼저 자신의 게시글과 관련하여 게시글 제목, 내용을 수정함.
 		boardDAO.modifyArticle(map);
 		
+		//게시글 제목과 내용수정에 성공하면 해당 게시글에 첨부된 파일들과 관련하여 처리를 진행해야함.
+		//1. 원래 이미지를 수정없이 남겨두는 경우.
+		//2. 원래 이미지를 다른 이미지로 변경한 경우.
+		//3. 원래 이미지를 삭제한 경우.
+		//4. 새로 추가한 이미지인 경우.
+		//총 4가지로 이미지를 분류할 수 있는데, 이중 1번의 경우만 FILE_TEMP_NAMES 파라미터에 임시 이름이 담겨있다.
+		//그외의 이미지들은 임시 이름이 없는데, 이렇게 임시 이름이 없는 2,3,4 이미지 파일들의 경우는 서버에 저장된 정보를
+		//전부 삭제하고 그에 맞게 다시 추가해야한다.
+		//1의 경우는 그대로 유지하면 된다.
 		String[] FILE_TEMP_NAMES = request.getParameter("FILE_TEMP_NAMES").split("\\*");
 		Iterator<String> iterator = request.getFileNames();
 		List<HashMap> fileNames = new ArrayList<HashMap>();
 		
+		//수정되지 않은 그대로의 이미지가 존재하는 경우에는 아래와 같이 잠시 TEMP 디렉토리로 옮긴후에
+		//앞서 언급한 2, 3, 4의 이미지들을 서버 저장소에서 제거한 후, 다시 TEMP 디렉토리에서 원래 디렉토리로 옮긴다.
 		if(request.getParameter("FILE_TEMP_NAMES")!=null&&!request.getParameter("FILE_TEMP_NAMES").equals("")) {
+			//임시로 1번 이미지들을 TEMP 디렉토리로 이동시킴.
 			for(int i=0;i<FILE_TEMP_NAMES.length;i++) {
 				File file = new File(REPOSITORY_PATH+"\\"+BOARD+"\\"+ARTICLE_ID+"\\"+FILE_TEMP_NAMES[i]);
 				if(file.exists()) {
@@ -59,8 +91,10 @@ public class BoardService {
 				}
 			}
 			
+			//해당 게시글 디렉토리를 완전히 삭제함.
 			FileUtils.deleteDirectory(new File(REPOSITORY_PATH+"\\"+BOARD+"\\"+ARTICLE_ID));
 			
+			//TEMP에서 해당 게시글 디렉토리로 다시 이미지 파일들을 이동시킴. 
 			for(int i=0;i<FILE_TEMP_NAMES.length;i++) {
 				File file = new File(REPOSITORY_PATH+"\\"+BOARD+"\\temp\\"+FILE_TEMP_NAMES[i]);
 				if(file.exists()) {
@@ -68,15 +102,20 @@ public class BoardService {
 				}
 			}
 		}else {
+			//수정되지 않은 이미지파일이 없는 경우, 전부 변경된 이미지이므로 디렉토리 자체를 일단 삭제함.
 			FileUtils.deleteDirectory(new File(REPOSITORY_PATH+"\\"+BOARD+"\\"+ARTICLE_ID));
 		}
 		
 		iterator = request.getFileNames();
 
+		//새로 변경되거나 추가된 이미지들에 대해서, 게시글 작성때와 마찬가지로 임시 이름을 할당하고
+		//서버에 해당 이름과 확장자를 가진 파일의 형태로 저장함.
 		while(iterator.hasNext()) {
 			String fileName = iterator.next();
 			MultipartFile mFile = request.getFile(fileName);
 			String originalFileName = mFile.getOriginalFilename();
+			
+			//게시글에 첨부된 이미지파일의 이름 중복을 회피하기위해, SHA512를 이용하여 무작위 이름을 할당함.
 			String tempFileName = SHA.DSHA512(SHA.getSalt(), SHA.getSalt()).substring(0, 32);
 			String fileExtension = mFile.getOriginalFilename().substring(mFile.getOriginalFilename().lastIndexOf("."));
 			File dest = new File(REPOSITORY_PATH+"\\"+BOARD+"\\"+ARTICLE_ID+"\\"+tempFileName+fileExtension);
@@ -88,13 +127,16 @@ public class BoardService {
 			mFile.transferTo(dest);
 		}
 
+		//수정되지않은 파일이 존재한다면 해당 파일을 제외한 모든 파일의 정보를 DB에서 수정함.
 		if(request.getParameter("FILE_TEMP_NAMES")!=null&&!request.getParameter("FILE_TEMP_NAMES").equals("")) {
 			map.put("FILE_TEMP_NAMES", FILE_TEMP_NAMES);
 			boardDAO.modifyFiles(map);
 		}else {
+			//모든 파일이 수정되었다면, 모든 파일의 정보를 DB에서 수정함.
 			boardDAO.modifyAllFiles(map);
 		}
 		
+		//새로 변경되거나 추가된 파일들에 대한 정보를 DB에 추가함.
 		if(fileNames.size()>0) {
 			map.put("fileNames", fileNames);
 			boardDAO.addFiles(map);
@@ -105,6 +147,13 @@ public class BoardService {
 		return result;
 	}
 	
+	
+	
+	
+	//============================================================================================
+	//게시글 목록을 얻어오는 서비스 메소드, LIST는 게시글들의 정보가 담긴 해시맵들을 저장한 리스트, TOTAL은 페이징을 위해
+	//조건에 맞는 게시글이 총 몇 개 존재하는지 그 개수를 나타내는 값임.
+	//============================================================================================
 	public HashMap listArticles(HashMap map) {
 		List list = boardDAO.listArticles(map);
 		int TOTAL = boardDAO.getArticlesCount(map);
@@ -120,14 +169,28 @@ public class BoardService {
 		}
 		return result;
 	}
-
+	
+	
+	
+	
+	//============================================================================================
+	//게시글의 상세정보를 읽고, 해당 게시글을 읽을 수 있도록 뷰를 리턴하는 메소드
+	//============================================================================================
 	public ModelAndView viewArticle(HashMap map) throws Exception{
+		//해당 게시글에 대한 게시글 ID, 게시글 제목, 게시글 내용, 작성 날짜 등등을 얻음.
 		HashMap articleVO = boardDAO.viewArticle(map);
+		
+		//게시글을 읽는데 문제가 없었다면, 해당 게시글에 첨부된 이미지 파일들에 대한 정보를 얻음.
 		map.put("BOARD_FILE", map.get("BOARD")+"_FILE");
 		List files = boardDAO.getFiles(map);
+		
+		//게시글을 읽는데 성공했다면, 해당 게시글의 조회수를 1 증가시킴.
 		if(articleVO!=null) {
 			boardDAO.updateViews(map);
 		}
+		
+		//해당 게시글의 상세정보를 보여주는 뷰를 리턴하는데, 해당 게시글 객체와 게시판 종류
+		//게시글에 첨부된 이미지 파일들의 정보가 담긴 리스트를 파라미터로 같이 전달함.
 		ModelAndView mav = new ModelAndView("viewArticle");
 		mav.addObject("articleVO", articleVO);
 		mav.addObject("BOARD", map.get("BOARD"));
@@ -135,14 +198,29 @@ public class BoardService {
 		return mav;
 	}
 	
+	
+	
+	
+	//============================================================================================
+	//게시글 삭제를 수행하는 메소드, 자신의 게시글만 삭제가 가능하며, 게시글 삭제에 성공하면 게시글에 첨부된 이미지 파일들의 정보와
+	//실제 이미지 파일들을 DB 및 서버의 저장소에서 제거함.
+	//============================================================================================
 	public ModelAndView deleteArticle(HashMap map) throws Exception{
+		
+		//게시글 삭제에 앞서, 특정 게시글을 삭제하면 해당 게시글을 포함하여 자신의 하위 게시글들은 모조리 삭제 되어야함.
+		//따라서 계층형 쿼리를 통해 먼저 연쇄적으로 삭제가 될 수 있도록 게시글 번호를 얻음.
 		List list = boardDAO.selectDeletedArticlesID(map);
 		map.put("BOARD_FILE", map.get("BOARD")+"_FILE");
 		map.put("LIST", list);
 		
+		//게시글에 첨부된 이미지 파일을 먼저 삭제함, 외래키 제약조건때문에 게시글이 먼저 삭제되면 오류가 발생함.
 		boardDAO.deleteFiles(map);
+		
+		//게시글을 삭제함.
 		boardDAO.deleteArticle(map);
 		
+		//게시글과 이미지 파일 정보삭제에 성공하면 실제로 서버에 저장된 이미지 파일들을 제거함.
+		//기존에 삭제되어야할 게시글들의 번호를 계층형 쿼리로 얻은 후, 리스트에 담았음.
 		for(int i=0;i<list.size();i++) {
 			File file = new File(REPOSITORY_PATH+"\\"+map.get("BOARD")+"\\"+list.get(i));
 			try {
@@ -153,6 +231,8 @@ public class BoardService {
 				e.printStackTrace();
 			}
 		}
+		
+		//게시글 삭제에 성공했으면, 해당 게시판으로 이동하도록 설정함.
 		ModelAndView mav = new ModelAndView("redirect:/board/boardForm.do");
 		mav.addObject("BOARD", map.get("BOARD"));
 		return mav;
