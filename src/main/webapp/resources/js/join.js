a802724e89bac0ef3aca6d99c5b60d99b110ce67
Blanc-et-noir <dign9060@gmail.com>
2022-02-26 13:33:34 +0900

    주석 추가

--- src/main/webapp/resources/js/join.js
+++ src/main/webapp/resources/js/join.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+	//============================================================================================
+	//해당 문자열이 알파벳과 숫자만으로 8자리 이상, 16자리 이하로 구성되어있는지 확인하는 메소드
+	//============================================================================================
 	function check(str) {
 		var regExp = /^[a-z0-9_]{8,16}$/;		
 		if(!regExp.test(str)) {
@@ -8,6 +11,13 @@ $(document).ready(function(){
 		} 
 	}
 
+	
+	
+	
+	
+	//============================================================================================
+	//해당 문자열이 010-0000-0000과 같이 전화번호의 형식으로 되어있는지 확인하는 메소드.
+	//============================================================================================
 	function checkPhone(str) {
 		var regExp = /^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$/;
 		if(!regExp.test(str)) {
@@ -17,6 +27,13 @@ $(document).ready(function(){
 		} 
 	}
 	
+	
+	
+	
+	
+	//============================================================================================
+	//해당 문자열이 aaaaaa@aaaaaa.com과 같이 이메일의 형식으로 되어있는지 확인하는 메소드.
+	//============================================================================================
 	function checkEmail(str) {
 		var regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 		if(!regExp.test(str)) {
@@ -25,6 +42,8 @@ $(document).ready(function(){
 			return true; 
 		} 
 	}
+	
+	//현재 날짜 이후로는 날짜를 선택할 수 없도록 함.
     var today = new Date();
     var year = today.getFullYear();
     var month,date;
@@ -38,13 +57,18 @@ $(document).ready(function(){
     }else{
     	date = today.getDate();
     }
-    
     var datelimit = year+"-"+month+"-"+date;
-
     $("#CUSTOMER_BDATE").attr({
     	"max":datelimit
     });
     
+    
+    
+    
+    
+    //============================================================================================
+    //정적 방식이 아니라 비동기 동적 방식으로 비밀번호 찾기 질문들을 리스트에 추가함.
+    //============================================================================================
     $.ajax({
     	"type":"POST",
     	"url":"/LibraryService/customer/getPasswordQuestionList.do",
@@ -59,6 +83,14 @@ $(document).ready(function(){
     	}
     });
     
+    
+    
+    
+    
+    
+    //============================================================================================
+    //이메일 인증코드 발급을 요청하는 메소드, 백엔드 서버에서 클라이언트가 입력한 해당 이메일로 이메일 인증 번호를 전달함.
+    //============================================================================================
     $(document).on("click","#EMAIL_AUTHCODE_BUTTON",function(){
     	$.ajax({
     		"url":"/LibraryService/mail/sendEmailAuthCode.do",
@@ -76,6 +108,13 @@ $(document).ready(function(){
     	})
     })
     
+    
+    
+    
+    
+    //============================================================================================
+    //클라이언트가 전달받은 인증코드를 제대로 입력했는지, 해당 이메일이 정말 존재하는지 인증코드를 통해 이메일을 인증하는 메소드.
+    //============================================================================================
     $(document).on("click","#EMAIL_AUTHEMAIL_BUTTON",function(){
     	$.ajax({
     		"url":"/LibraryService/customer/authenticateEmail.do",
@@ -93,6 +132,13 @@ $(document).ready(function(){
     	})
     })
     
+    
+    
+    
+    
+    //============================================================================================
+    //회원가입 버튼을 클릭하면 회원가입을 백엔드 서버에 요청하는 메소드.
+    //============================================================================================
     $(document).on("click","#JOIN_BUTTON",function(){
     	var CUSTOMER_ID = $("#CUSTOMER_ID").val();
     	var CUSTOMER_PW = $("#CUSTOMER_PW").val();
@@ -102,6 +148,8 @@ $(document).ready(function(){
     	var CUSTOMER_EMAIL = $("#CUSTOMER_EMAIL").val();
     	var CUSTOMER_ADDRESS = $("#CUSTOMER_ADDRESS").val();
     	var CUSTOMER_BDATE = $("#CUSTOMER_BDATE").val();
+    	
+    	//KIND_NUMBER가 0이면 관리자, 1이면 고객임.
     	var KIND_NUMBER = $("#KIND_NUMBER").val();
 		var PASSWORD_QUESTION_LIST_ID = $("#PASSWORD_QUESTION_LIST_ID").val();
 		var PASSWORD_HINT_ANSWER = $("#PASSWORD_HINT_ANSWER").val();
@@ -123,14 +171,34 @@ $(document).ready(function(){
     	}else if(CUSTOMER_ADDRESS.length == 0){
     		alert("주소는 공백일 수 없습니다.");
     	}else{
+    		
+    		
+    		
+    		
+    		//============================================================================================
+    		//백엔드 서버로부터 공개키를 요청하는 메소드. 이에 대응되는 비밀키는 백엔드 서버의 세션에 저장됨.
+    		//============================================================================================
         	$.ajax({
         		"url":"/LibraryService/customer/getPublicKey.do",
         		"dataType":"text",
         		"type":"POST",
         		"success":function(result){
+        			
+        			
+        			
+        			
+        			//전달받은 공개키로 비밀번호와 비밀번호 찾기 질문에 대한 답을 암호화 함.
         			var PUBLICKEY = result;
             		CUSTOMER_PW = encryptByRSA2048(CUSTOMER_PW,PUBLICKEY);
             		PASSWORD_HINT_ANSWER = encryptByRSA2048(PASSWORD_HINT_ANSWER,PUBLICKEY);
+            		
+            		
+            		
+            		
+            		
+            		//============================================================================================
+            		//해당 사용자 정보로 회원가입을 요청하는 메소드.
+            		//============================================================================================
             		$.ajax({
             			"type":"POST",
             			"url":"/LibraryService/customer/join.do",
--- src/main/webapp/resources/js/receiveMessage.js
+++ src/main/webapp/resources/js/receiveMessage.js
@@ -1,3 +1,6 @@
+//============================================================================================
+//해당 날짜가 비교적 최근인지 아닌지 구분하는 메소드.
+//============================================================================================
 function isRecent(MESSAGE_DATE_STRING){
 	var temp1 = MESSAGE_DATE_STRING.split(" ");
 	var temp2 = temp1[0].split(".");
@@ -13,15 +16,25 @@ function isRecent(MESSAGE_DATE_STRING){
 }
 
 $(document).ready(function(){
-	
+	//기본적으로 수신 메세지함을 검색하며, 메세지 송수신 날짜를 기준으로 내림차순으로 검색함.
 	var MESSAGE_BOX = "MESSAGE_RECEIVED";
 	var ORDERBY = "MESSAGE_DATE DESC";
 	var section=1, page=1;
+	
+	//메세지 삭제시에 모든 메세지를 클릭했는지 아닌지 여부를 나타내는 변수.
 	var checkAll = false;
 	var SEARCH ="";
 	
+	//페이지 로딩시 즉시 비동기식으로 메세지 목록을 요청함.
 	receiveMessage(MESSAGE_BOX, ORDERBY, section, page);
 	
+	
+	
+	
+	
+	//============================================================================================
+	//메세지 목록을 요청하는 메소드, 최대 10개의 메세지를 한번에 요청할 수 있음.
+	//============================================================================================
 	function receiveMessage(MESSAGE_BOX, ORDERBY, section, page){
 		$.ajax({
 			"url":"/LibraryService/message/receiveMessage.do",
@@ -35,14 +48,16 @@ $(document).ready(function(){
 				"SEARCH":SEARCH
 			},
 			"success":function(result){
+				//메세지 체크상태를 해제함.
 				checkAll=false;
 				if(result.FLAG=="TRUE"){
 					var TOTAL = result.TOTAL;
 					var LIST = result.LIST;
 					var i;
-					
 					$("#LIST").empty();
 					$("#LIST").append("<tr class='row'><th class='col' id='SELECT_ALL_BUTTON'>전체</th><th class='col' id='FLAG'>"+(MESSAGE_BOX=="MESSAGE_RECEIVED"?"송신자":"수신자")+"</th><th id='TITLE' class='col'>제목</th><th id='DATE' class='col'>보낸시각</th></tr>");
+					
+
 					for(i=0;i<LIST.length;i++){
 						var recent = isRecent(LIST[i].MESSAGE_DATE_STRING);
 						var NEW = recent==true?" <span class='new'>[new]</span>":"";
@@ -50,6 +65,8 @@ $(document).ready(function(){
 						var tr = $("<tr></tr>");
 						
 						tr.append("<td><input type='checkbox' class='check' value='"+LIST[i].MESSAGE_ID+"'></td>");
+						
+						//송신함인지 수신함인지에 따라 각각 수신자, 송신자가 누구인지 나타냄.
 						if(MESSAGE_BOX == "MESSAGE_RECEIVED"){
 							tr.append("<td>"+LIST[i].SENDER_ID+"</td>");
 						}else{
@@ -58,6 +75,9 @@ $(document).ready(function(){
 						
 						tr.append("<td><a href='/LibraryService/message/readMessage.do?MESSAGE_BOX="+MESSAGE_BOX+"&MESSAGE_ID="+LIST[i].MESSAGE_ID+"'>"+LIST[i].MESSAGE_TITLE+NEW+"</a></td>");
 						
+						
+						//메세지 목록 수신에 성공했을때, 비교적 최신의 메세지라면 new 키워드를 추가하고, 해당 메세지의 송, 수신 시간을 표시함.
+						//만약 비교적 최신의 메세지가 아니라면, 송, 수신 날짜를 표기함.
 						if(recent){
 							tr.append("<td>"+temp[1]+"</td>");
 						}else{
@@ -81,6 +101,13 @@ $(document).ready(function(){
 		});
 	}
 	
+	
+	
+	
+	
+	//============================================================================================
+	//메세지 삭제를 요청하는 메소드, 선택된 메세지를 일괄적으로 삭제함.
+	//============================================================================================
 	function deleteMessage(MESSAGE_BOX){
 		var MESSAGE = $(".check:checked");
 		var MESSAGE_ID="";
@@ -89,6 +116,8 @@ $(document).ready(function(){
 			alert("삭제할 메세지를 선택해야합니다.");
 			return;
 		}
+		
+		//체크된 메세지들의 아이디를 공백문자로 구분하여 하나의 문자열로 합침.
 		for(i=0; i<MESSAGE.length; i++){
 			if(i!=MESSAGE.length-1){
 				MESSAGE_ID+=$(MESSAGE[i]).attr("value")+" ";
@@ -96,6 +125,13 @@ $(document).ready(function(){
 				MESSAGE_ID+=$(MESSAGE[i]).attr("value");
 			}
 		}
+		
+		
+		
+		
+		//============================================================================================
+		//백엔드 서버에 해당 메세지들의 삭제를 요청함.
+		//============================================================================================
 		$.ajax({
 			"url":"/LibraryService/message/deleteMessage.do",
 			"dataType":"JSON",
@@ -105,7 +141,10 @@ $(document).ready(function(){
 				"MESSAGE_ID":MESSAGE_ID
 			},
 			"success":function(result){
+				//메세지 삭제에 성공하면 메세지를 다시 읽음.
 				if(result.FLAG=="TRUE"){
+					section=1;
+					page=1;
 					receiveMessage(MESSAGE_BOX, ORDERBY, section, page);
 				}else{
 					$("#LIST").empty();
@@ -122,6 +161,14 @@ $(document).ready(function(){
 		});
 	}
 	
+	
+	
+	
+	
+	
+	//============================================================================================
+	//해당 조건에 맞는 메세지들의 개수를 받아 페이징 처리하는 메소드.
+	//============================================================================================
 	function paging(TOTAL){
 		var maxSection = Math.ceil(TOTAL/100);
 		var maxPage = Math.ceil((TOTAL-(section-1)*100)/10)>=10?10:Math.ceil((TOTAL-(section-1)*100)/10);
@@ -131,6 +178,9 @@ $(document).ready(function(){
 		$("#paging").empty();
 		
 		var tag;
+		
+		//섹션이 2이상이라면, 101번째 이상의 게시글을 조회하고 있는 것이므로
+		//그 이전 100개의 리스트를 얻을 수 있도록 이전버튼을 생성함.
 		if(section>=2){
 			tag = $("<a id='prev'>이전</a>");
 			$("#paging").on("click","#prev",function(){
@@ -140,6 +190,8 @@ $(document).ready(function(){
 			})
 			$("#paging").append(tag);
 		}
+		
+		//해당 섹션에 존재하는 페이지수에 따라 페이징을 수행함.
 		for(var i=1; i<=maxPage;i++){
 			$("#paging").append($("<a id='idx"+i+"' value='"+i+"'>"+i+"</a>"));
 			
@@ -150,6 +202,8 @@ $(document).ready(function(){
 		}
 		$("#paging a").removeClass("active");
 		$("#idx"+page).addClass("active");
+		
+		//아직 마지막 섹션이 아니라면, 다음 섹션으로 넘어갈 수 있도록 다음 버튼을 생성함.
 		if(section<maxSection){
 			tag = $("<a id='next'>다음</a>");
 			$("#paging").on("click","#next",function(){
@@ -161,6 +215,14 @@ $(document).ready(function(){
 		}
 	}
 	
+	
+	
+	
+	
+	//============================================================================================
+	//메세지 전체 선택 클릭시, 해당 페이지에 존재하는 모든 메세지들을 체크함.
+	//현재 모든 메세지 체크 상태라면, 모든 메세지 체크를 해제함.
+	//============================================================================================
 	$(document).on("click","#SELECT_ALL_BUTTON",function(){
 		if(checkAll == false){
 			checkAll=true;
@@ -169,10 +231,26 @@ $(document).ready(function(){
 			checkAll=false;
 			$(".check").prop("checked",false);
 		}
-	})
+	});
+	
+	
+	
+	
+	
+	//============================================================================================
+	//선택된 메세지들을 삭제하는 이벤트를 등록함.
+	//============================================================================================
 	$(document).on("click","#DELETE_BUTTON",function(){
 		deleteMessage(MESSAGE_BOX);
 	})
+	
+	
+	
+	
+	
+	//============================================================================================
+	//메세지 송신함의 메세지를 조회하도록 설정하는 이벤트를 등록함.
+	//============================================================================================
 	$(document).on("click","#MESSAGE_SENT",function(){
 		page = 1;
 		section = 1;
@@ -180,6 +258,14 @@ $(document).ready(function(){
 		ORDERBY = "MESSAGE_DATE DESC";
 		receiveMessage(MESSAGE_BOX, ORDERBY, section, page);
 	});
+	
+	
+	
+	
+	
+	//============================================================================================
+	//메세지 수신함의 메세지를 조회하도록 설정하는 이벤트를 등록함.
+	//============================================================================================
 	$(document).on("click","#MESSAGE_RECEIVED",function(){
 		page = 1;
 		section = 1;
@@ -187,6 +273,14 @@ $(document).ready(function(){
 		ORDERBY = "MESSAGE_DATE DESC";
 		receiveMessage(MESSAGE_BOX, ORDERBY, section, page);
 	});
+	
+	
+	
+	
+	
+	//============================================================================================
+	//메세지 검색 이벤트를 등록함. 해당 조건에 맞는 메세지들을 얻음.
+	//============================================================================================
 	$(document).on("click","#SEARCH_BUTTON",function(){
 		page = 1;
 		section = 1;
@@ -194,6 +288,14 @@ $(document).ready(function(){
 		ORDERBY = "MESSAGE_DATE DESC";
 		receiveMessage(MESSAGE_BOX, ORDERBY, section, page);
 	});
+	
+	
+	
+	
+	
+	//============================================================================================
+	//메세지들을 송, 수신 날짜를 기준으로 오름차 혹은 내림차순으로 조회하는 이벤트를 등록함.
+	//============================================================================================
 	$(document).on("click","#DATE",function(){
 		if(ORDERBY == "MESSAGE_DATE DESC"){
 			ORDERBY = "MESSAGE_DATE ASC";
@@ -202,6 +304,14 @@ $(document).ready(function(){
 		}
 		receiveMessage(MESSAGE_BOX, ORDERBY, section, page);
 	});
+	
+	
+	
+	
+	
+	//============================================================================================
+	//메세지들을 메세지 제목을 기준으로 오름차 혹은 내림차순으로 조회하는 이벤트를 등록함.
+	//============================================================================================
 	$(document).on("click","#TITLE",function(){
 		if(ORDERBY == "MESSAGE_TITLE ASC"){
 			ORDERBY = "MESSAGE_TITLE DESC";
@@ -210,6 +320,14 @@ $(document).ready(function(){
 		}
 		receiveMessage(MESSAGE_BOX, ORDERBY, section, page);
 	});
+	
+	
+	
+	
+	
+	//============================================================================================
+	//메세지들을 송신자 혹은 수신자이름으로 오름차 혹은 내림차 순으로 조회하는 이벤트를 등록함.
+	//============================================================================================
 	$(document).on("click","#FLAG",function(){
 		if(MESSAGE_BOX == "MESSAGE_RECEIVED"){
 			if(ORDERBY == "SENDER_ID ASC"){
